aef45247dfd165e6cd655214571ba9a401712746
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2022-03-07 11:08:53 -0400

    init calculadora

---
+++ swing/src/test/java/br/com/calculadora/AppTest.java
@@ -0,0 +1,14 @@
+package br.com.calculadora;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
